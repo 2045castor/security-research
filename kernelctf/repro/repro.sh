c77374ef23c0b23a0b4df94c9a0c2727320cc3b2
Zi-Xuan Fu <r33s3n6@gmail.com>
2026-05-08 18:06:38 +0800

    fix repro sh, adjust unmovable param

--- kernelctf/repro/repro.sh
+++ kernelctf/repro/repro.sh
@@ -251,7 +251,7 @@ else
     # echo "QEMU_OUTPUT_B64=$(cat $QEMU_TXT|base64 -w0)" >> "$GITHUB_OUTPUT"
     echo "RUN_TIME=$(expr $(date +%s) - $START_TIME)" >> "$GITHUB_OUTPUT"
     
-    if grep -q $FLAG $QEMU_TXT; then
+    if grep -q $FLAG repro_log_$TRY_ID.txt; then
         echo "Got the flag! Congrats!"
         exit 0
     else
